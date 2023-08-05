2adf3ea07a3543439cce6aaad86098f2b825c73e
Hector Chocobar-Torrejon <45291267+hchocobar@users.noreply.github.com>
2023-08-05 07:46:01 -0300

    Update webpack.config.js
    elimnando prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const PrettierPlugin = require("./_utils/prettier.js");
+// const PrettierPlugin = require("./_utils/prettier.js");
 const cleanStack = require("./_utils/clean-stack.js");
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
@@ -74,8 +74,8 @@ module.exports = {
         filename: "index.html",
         template: "src/index.html"
     }),
-    new PrettierPlugin({
-        failSilently: true
-    }),
+    // new PrettierPlugin({
+    //    failSilently: true
+    // }),
   ]
 };
