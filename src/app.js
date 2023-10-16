1caad8ea9a87676bcd03d94e575c57bd7e67d08c
Héctor Chocobar-Torrejón <45291267+hchocobar@users.noreply.github.com>
2023-10-16 16:22:53 +0000

    class 3

--- src/app.js
+++ src/app.js
@@ -1,43 +1,11 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+
 window.onload = function () {
-  let person1 = {
-    name: 'Jhon',
-    image: 'https://img.freepik.com/foto-gratis/apuesto-joven-brazos-cruzados-sobre-fondo-blanco_23-2148222620.jpg?size=626&ext=jpg',
-    age: 35,
-    lastname: 'Doe',
-    gender: 'male',
-    sports: ['badgminton', 'tennis', 'swimming']
-  };
-
-  let person2 = {
-    name: 'Jane',
-    image: 'https://img.freepik.com/foto-gratis/mujer-bastante-joven-feliz-que-presenta-camara-parque-ciudad_1262-19158.jpg',
-    age: 30,
-    lastname: 'Doe',
-    gender: 'female',
-    sports: ['basketball', 'skate',]
-  };
-
-  let person3 = {
-    name: 'Jimmy',
-    image: 'https://img.freepik.com/vector-gratis/lindo-nino-sonriente-feliz-aislado-blanco_1308-32243.jpg?size=626&ext=jpg&ga=GA1.1.1406716074.1697051293&semt=sph',
-    age: 10,
-    lastname: 'Doe',
-    gender: 'male',
-    sports: ['soccer', 'baseball', 'rugby']
-  };
-
-  let family = {
-    lastname: 'Doe',
-    members: [person1, person2, person3]
-  };
-
   // Declaramos la Función (definimos)
   function allSports(aFamily) {
     
@@ -76,9 +44,40 @@ window.onload = function () {
                   </div>`
 
     })
+
+    let person1 = {
+    name: 'Jhon',
+    image: 'https://img.freepik.com/foto-gratis/apuesto-joven-brazos-cruzados-sobre-fondo-blanco_23-2148222620.jpg?size=626&ext=jpg',
+    age: 35,
+    lastname: 'Doe',
+    gender: 'male',
+    sports: ['badgminton', 'tennis', 'swimming']
+  };
+  let person2 = {
+    name: 'Jane',
+    image: 'https://img.freepik.com/foto-gratis/mujer-bastante-joven-feliz-que-presenta-camara-parque-ciudad_1262-19158.jpg',
+    age: 30,
+    lastname: 'Doe',
+    gender: 'female',
+    sports: ['basketball', 'skate',]
+  };
+  let person3 = {
+    name: 'Jimmy',
+    image: 'https://img.freepik.com/vector-gratis/lindo-nino-sonriente-feliz-aislado-blanco_1308-32243.jpg?size=626&ext=jpg&ga=GA1.1.1406716074.1697051293&semt=sph',
+    age: 10,
+    lastname: 'Doe',
+    gender: 'male',
+    sports: ['soccer', 'baseball', 'rugby']
+  };
+  let family = {
+    lastname: 'Doe',
+    members: [person1, person2, person3]
+  };
+
     console.log(result);
     return result;
   }
+
 
   // Invocar la Función (llamarla, ejecutarla)
   // console.log(allSports(family.members))
---
+++ src/assets/js-vanilla/functions-10.js
@@ -0,0 +1,29 @@
+/* Remove Vowels
+
+Toma nota:
+
+Las funciones flecha permiten una sintaxis corta para escribir expresiones de funciones.
+
+const multi = (x, y) => x * y;
+console.log(multi(2,2));
+
+📝 Instrucciones:
+
+  1. Crea una función flecha llamada rapid que tomará un string.
+  2. Recorre (loop) el texto y elimina todas las vocales.
+  3. Coloca en mayúscula todas las consonantes.
+  4. Devuelve el nuevo string.
+
+💡 Pista:
+
+    Implementa estos métodos en tu código para retornar el resultado correcto:
+    .toLowerCase() y .toUpperCase()
+*/
+// Your code goes here
+const rapid = (myString) => {
+  return myString.toLowerCase()
+}
+
+// Work above this line; do not change code below
+let str = "John";
+console.log(rapid(str));
