0a28c2271519945b34fff71cd23e9a30fd28180f
Hector Chocobar-Torrejon <45291267+hchocobar@users.noreply.github.com>
2023-12-07 20:33:00 +0000

    feat: clase 1 - enviando listas desde javascript a html

--- src/app.js
+++ src/app.js
@@ -1,11 +1,58 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
+  const person1 = {
+    name: 'Joe',
+    age: 35,
+    sports: ['Hockey', 'Soccer', 'Baseball'],
+    image: ''
+  }
+  const person2 = {
+    name: 'Jane',
+    age: 30,
+    sports: ['Tennis', 'Basketball', 'Voleyball', 'Bike'],
+    image: ''
+  }
+  const person3 = {
+    name: 'Jimmy',
+    age: 5,
+    sports: ['Jugar', 'Correr'],
+    image: ''
+  }
+  let family = {
+    lastName: 'Doe',
+    member: [person1, person2, person3]
+  }
+  
+  const tag = document.querySelector('#app')  // document.getElementById('app')
+  console.log('Result');
+  /* console.log(family.member[0].name)
+  console.log(family.member[1].name)
+  console.log(family.member[2].name)
+  console.log(family.member[0].sports[1])
+  console.log(family.member[2].sports[0]) */
+  console.log(family.member)
+  family.member.map((item) => {
+    // Utilizando concatenación
+    tag.innerHTML += '<h2 class="mt-4">' + item.name + '</h2><ul class="list-group">'
+    console.log(item.name)
+    item.sports.map((element) => {
+      // Utilizando string literal
+      tag.innerHTML += `<li class="list-group-item">${element}</li>`
+      // `string text ${expression} string text`
+      console.log('-', element)
+    })
+    tag.innerHTML += '</ul>'
+  })
+
+  console.log(tag.innerHTML);
+  // tag.innerHTML = '<ul><li>Hockey</li><li>Soccer</li><li>Baseball</li></ul>'
+
+
   console.log("Hello Rigo from the console!");
 };
--- src/assets/js-vanilla/begginer-24.js
+++ src/assets/js-vanilla/begginer-24.js
@@ -49,3 +49,83 @@
   
   // Do not make changes below:
   console.log(addAllFamilyLuckyNumbers(family.members));
+
+
+    // Declaramos la Función (definimos)
+    function allSports(aFamily) {
+      /* 
+      // let result = [];    for (const i in aFamily) {
+            const element = aFamily[i];
+            // console.log('valor de i: ', i);
+            // console.log(aFamily[i].sports);
+            result += `<div class="card">
+                          <img src=${element.image} class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h5 class="card-title">${element.name} ${element.lastname}</h5>
+                            <p class="card-text">age ${element.age}</p>
+                            <p class="card-text">gender ${element.gender}</p>
+                            <ul class="list-group">`;
+            for (const j in aFamily[i].sports) {
+                const item = aFamily[i].sports[j];
+                result += `<li class="list-group-item">${item}</li>`;
+              }
+  
+            result += `</ul>
+            <a href="#" class="btn btn-success">Ver detalles</a>
+                    </div>
+                  </div>`
+          } */
+      let result = '';
+      aFamily.map((element, id) => {
+        result += `<div class="card key=${id}">
+                      <img src=${element.image} class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h5 class="card-title">${element.name} ${element.lastname}</h5>
+                        <p class="card-text">age ${element.age}</p>
+                        <p class="card-text">gender ${element.gender}</p>
+                        <a href="#" class="btn btn-success">Ver detalles</a>
+                      </div>
+                    </div>`
+  
+      })
+      console.log(result);
+      return result;
+    }
+    
+    let person1 = {
+      name: 'Jhon',
+      image: 'https://img.freepik.com/foto-gratis/apuesto-joven-brazos-cruzados-sobre-fondo-blanco_23-2148222620.jpg?size=626&ext=jpg',
+      age: 35,
+      lastname: 'Doe',
+      gender: 'male',
+      sports: ['badgminton', 'tennis', 'swimming']
+    };
+    let person2 = {
+      name: 'Jane',
+      image: 'https://img.freepik.com/foto-gratis/mujer-bastante-joven-feliz-que-presenta-camara-parque-ciudad_1262-19158.jpg',
+      age: 30,
+      lastname: 'Doe',
+      gender: 'female',
+      sports: ['basketball', 'skate',]
+    };
+    let person3 = {
+      name: 'Jimmy',
+      image: 'https://img.freepik.com/vector-gratis/lindo-nino-sonriente-feliz-aislado-blanco_1308-32243.jpg?size=626&ext=jpg&ga=GA1.1.1406716074.1697051293&semt=sph',
+      age: 10,
+      lastname: 'Doe',
+      gender: 'male',
+      sports: ['soccer', 'baseball', 'rugby']
+    };
+    let family = {
+      lastname: 'Doe',
+      members: [person1, person2, person3]
+    };
+  
+    // Invocar la Función (llamarla, ejecutarla)
+    // console.log(allSports(family.members))
+  
+    // Capturar un elemento HTML - tag, .class, #id
+    let myTag = document.querySelector('#sports');
+    myTag.innerHTML += allSports(family.members);
+  
+    console.log("Hello JavaScript from the console!");
