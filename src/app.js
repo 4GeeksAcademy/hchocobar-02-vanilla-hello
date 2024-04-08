bc9c9738b7bae454423db5fca9078069141ef546
Héctor Chocobar-Torrejón <45291267+hchocobar@users.noreply.github.com>
2024-04-08 19:51:12 +0000

    feat: random card generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-import jobs from "./assets/js-vanilla/jobs.json";
 
+function newCard() {
+  let suits = ['♦', '♥', '♠', '♣'];
+  let numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+  let indexSuits = Math.floor(Math.random() * suits.length);
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  // Mostrar los palos y los números
+  document.querySelector('#top').innerHTML = suits[indexSuits];
+  document.querySelector('#end').innerHTML = suits[indexSuits];
+  document.querySelector('#number').innerHTML = numbers[indexNumbers];
+  // Mostrar los colores adecuados
+  let newColor = suits[indexSuits] == '♦' || suits[indexSuits] == '♥' ? 'text-danger' : 'text-dark';
+  document.querySelector('#top').className = newColor;
+  document.querySelector('#end').className = newColor;
+  document.querySelector('#number').className = newColor;
+  console.log('hola')
+}
 
-window.onload = function () {
-  console.log('Hello Spain-65')
-};
+setInterval(() => newCard(), 5000)
+
+document.querySelector('#newCard').addEventListener('click', () => newCard())
+
+window.onload = function () {newCard();};
